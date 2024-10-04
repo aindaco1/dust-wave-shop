12ec048618f53b1719fdb61269a5ad970948689f
jayrenteria <jayevanrenteria@gmail.com>
2024-10-04 15:00:58 -0600

    Fixes price button issue

--- assets/main.js
+++ assets/main.js
@@ -1,19 +1,10 @@
 window.addEventListener('DOMContentLoaded', function(event) {
-    document.addEventListener('change', function(evt){
-        console.log(evt.target.classList)
-        if(evt.target.classList.contains('qty')){
+    document.addEventListener('change', function(evt) {
+        console.log(evt.target.classList[0])
+        if (evt.target.classList[0] === 'qty') {
             var button = evt.target.parentNode.querySelector('.buy-button')
             var qty = parseInt(evt.target.value)
             button.setAttribute('data-item-quantity', qty)
-            var label = button.innerHTML
-            label = label.replace(/\d+/, evt.target.value)
-            if(qty > 1){
-                label = label.replace(/copy/, "copies")
-            }
-            else{
-                label = label.replace(/copies/, "copy")
-            }
-            button.innerHTML = label;
         }
     })
 });
@@ -24,17 +15,17 @@ const increment = document.querySelector(".increment");
 const decrement = document.querySelector(".decrement");
 
 quantity.addEventListener("change", () => {
-  addToCart.setAttribute("data-item-quantity", quantity.value);
+    addToCart.setAttribute("data-item-quantity", quantity.value);
 });
 
 increment.addEventListener("click", () => {
-  const newQuantity = (parseInt(quantity.value) || 0) + 1;
-  quantity.value = newQuantity;
-  addToCart.setAttribute("data-item-quantity", newQuantity);
+    const newQuantity = (parseInt(quantity.value) || 0) + 1;
+    quantity.value = newQuantity;
+    addToCart.setAttribute("data-item-quantity", newQuantity);
 });
 
 decrement.addEventListener("click", () => {
-  const newQuantity = (parseInt(quantity.value) || 0) - 1;
-  quantity.value = newQuantity;
-  addToCart.setAttribute("data-item-quantity", newQuantity);
+    const newQuantity = (parseInt(quantity.value) || 0) - 1;
+    quantity.value = newQuantity;
+    addToCart.setAttribute("data-item-quantity", newQuantity);
 });
