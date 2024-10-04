4aa5bbea9e4e3ee8a84a3843a213b6f67eb8b0f2
jayrenteria <jayevanrenteria@gmail.com>
2024-10-04 15:23:19 -0600

    Move into dom loaded

--- assets/main.js
+++ assets/main.js
@@ -7,25 +7,30 @@ window.addEventListener('DOMContentLoaded', function(event) {
             button.setAttribute('data-item-quantity', qty)
         }
     })
+
+    const addToCart = document.querySelector(".snipcart-add-item");
+    const quantity = document.querySelector(".qty");
+    const increment = document.querySelector(".increment");
+    const decrement = document.querySelector(".decrement");
+
+    quantity.addEventListener("change", () => {
+        addToCart.setAttribute("data-item-quantity", quantity.value);
+    });
+
+    if (increment) {
+        increment.addEventListener("click", () => {
+            const newQuantity = (parseInt(quantity.value) || 0) + 1;
+            quantity.value = newQuantity;
+            addToCart.setAttribute("data-item-quantity", newQuantity);
+        });
+    }
+
+    if (decrement) {
+        decrement.addEventListener("click", () => {
+            const newQuantity = (parseInt(quantity.value) || 0) - 1;
+            quantity.value = newQuantity;
+            addToCart.setAttribute("data-item-quantity", newQuantity);
+        });
+    }
 });
 
-const addToCart = document.querySelector(".snipcart-add-item");
-const quantity = document.querySelector(".qty");
-const increment = document.querySelector(".increment");
-const decrement = document.querySelector(".decrement");
-
-quantity.addEventListener("change", () => {
-    addToCart.setAttribute("data-item-quantity", quantity.value);
-});
-
-increment.addEventListener("click", () => {
-    const newQuantity = (parseInt(quantity.value) || 0) + 1;
-    quantity.value = newQuantity;
-    addToCart.setAttribute("data-item-quantity", newQuantity);
-});
-
-decrement.addEventListener("click", () => {
-    const newQuantity = (parseInt(quantity.value) || 0) - 1;
-    quantity.value = newQuantity;
-    addToCart.setAttribute("data-item-quantity", newQuantity);
-});
